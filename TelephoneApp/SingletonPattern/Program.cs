aa0ffaf8c2a64062823cfd62f01f9525faa48e4f
Aseka <grooves97@gmail.com>
2019-07-08 23:16:45 +0600

    Added Singleton and Prototype

---
+++ TelephoneApp/SingletonPattern/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class Singleton
+    {
+        private Singleton() { }
+        private static Singleton _instance;
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+                _instance = new Singleton();
+            return _instance;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            if (s1 == s2)
+                Console.WriteLine("true");
+            else
+                Console.WriteLine("false");
+        }
+    }
+}
